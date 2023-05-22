2e846a5122e5a7fad5231d301a48bc42d1ae8d57
Ogu1208 <kdasunb@naver.com>
2023-05-22 17:00:44 +0900

    [feat]: create CPUSchedulingAlgorithm
    CPU 스케줄링 알고리즘  클래스들의 abstract 클래스 생성. 
    - getter/setter
    - boolean addRow(Row row)
    - abstract void process();  -> 각 알고리즘에서 구현할 추상메서드

---
+++ src/CPUSchedulingAlgorithm.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class CPUSchedulingAlgorithm {
+    private final List<Row> rows;
+    private final List<Event> timeline;
+    private int timeQuantum;
+
+    public CPUSchedulingAlgorithm() {
+        rows = new ArrayList<>();
+        timeline = new ArrayList<>();
+    }
+
+    public boolean addRow(Row row)
+    {
+        return rows.add(row);
+    }  // 행 추가
+    public abstract void process();  // process 함수 (abstract)
+
+    public double getAverageWaitingTime()
+    {
+        double avg = 0.0;
+
+        for (Row row : rows)
+        {
+            avg += row.getWaitingTime();
+        }
+
+        return avg / rows.size();
+    }
+
+    public double getAverageTurnAroundTime()
+    {
+        double avg = 0.0;
+
+        for (Row row : rows)
+        {
+            avg += row.getTurnaroundTime();
+        }
+
+        return avg / rows.size();
+    }
+
+    /*
+
+    public double getAverageResponseTime()
+    {
+        double avg = 0.0;
+
+        for (Row row : rows)
+        {
+            avg += row.getResponseTime();
+        }
+
+        return avg / rows.size();
+    }
+     */
+
+    // getter
+
+    // process의 프로세스 이름과 같은 row를 반환
+    public Row getRow(String process)
+    {
+        for (Row row : rows)
+        {
+            if (row.getProcessName().equals(process))
+            {
+                return row;
+            }
+        }
+
+        return null;
+    }
+
+    // row의 process 이름과 같은 event를 반환
+    public Event getEvent(Row row)
+    {
+        for (Event event : timeline)
+        {
+            if (row.getProcessName().equals(event.getProcessName()))
+            {
+                return event;
+            }
+        }
+
+        return null;
+    }
+
+
+
+    public List<Row> getRows() {
+        return rows;
+    }
+
+    public List<Event> getTimeline() {
+        return timeline;
+    }
+
+    public int getTimeQuantum() {
+        return timeQuantum;
+    }
+
+
+    // setter
+    public void setTimeQuantum(int timeQuantum) {
+        this.timeQuantum = timeQuantum;
+    }
+}
